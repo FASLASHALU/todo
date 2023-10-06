1465a755281ab326b7e50b1bf1193a19848becbb
FASLA V M <141605898+FASLASHALU@users.noreply.github.com>
2023-10-06 10:00:51 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:todo/home.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('mybox');
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: home(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
